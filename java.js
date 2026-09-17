003ed66f896a821b41641ee4fc90bba070885a43
Victor Cardoso <victorcardosods7@gmail.com>
2026-09-17 20:18:52 -0300

    UPDATE

--- java.js
+++ java.js
@@ -4,3 +4,24 @@ const button = document.querySelector("#buttonBtn");
 button.addEventListener("click", function () {
     page.classList.toggle("light-mode");
 });
+
+
+const buttond = document.querySelector("#carsButton");
+const carsSection = document.querySelector("#cars");
+
+buttond.addEventListener("click", function(){
+
+    carsSection.scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+const buttonb = document.querySelector("#movieB");
+const movie = document.querySelector("#movieS");
+
+buttonb.addEventListener("click", function(){
+
+    carsSection.scrollIntoView({
+        behavior: "smooth"
+    });
+});
